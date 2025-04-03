4ceede1f6445749972a89a39d0beed489c1f18b6
lordwisetech <104361385+lordwisetech@users.noreply.github.com>
2025-04-03 08:28:15 -0700

    Update manage.py

--- manage.py
+++ manage.py
@@ -2,6 +2,9 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from django.core.management import execute_from_command_line
+
+
 
 
 def main():
@@ -19,3 +22,7 @@ def main():
 
 if __name__ == '__main__':
     main()
+    
+    # Use the PORT environment variable or default to 8000 if not provided
+    port = os.environ.get('PORT', 8000)
+    execute_from_command_line(['manage.py', 'runserver', f'0.0.0.0:{port}'])
